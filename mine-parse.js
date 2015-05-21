bd03531180d407e707e81400dc38de20958c5569
Alfonso Garnett <lentiformnucleus@gmail.com>
2015-05-21 17:07:16 -0500

    Update mine-parse.js

--- mine-parse.js
+++ mine-parse.js
@@ -1,64 +1,38 @@
 (function () {
 
-    var _id = 0,
+    'use strict';
+
+    var currId = 0,
         obfuscators = {},
-        alreadyParsed = [];
+        alreadyParsed = [],
+        styleMap = {
+            '§0': 'color:#000000',
+            '§1': 'color:#0000AA',
+            '§2': 'color:#00AA00',
+            '§3': 'color:#00AAAA',
+            '§4': 'color:#AA0000',
+            '§5': 'color:#AA00AA',
+            '§6': 'color:#FFAA00',
+            '§7': 'color:#AAAAAA',
+            '§8': 'color:#555555',
+            '§9': 'color:#5555FF',
+            '§a': 'color:#55FF55',
+            '§b': 'color:#55FFFF',
+            '§c': 'color:#FF5555',
+            '§d': 'color:#FF55FF',
+            '§e': 'color:#FFFF55',
+            '§f': 'color:#FFFFFF',
+            '§l': 'font-weight:bold',
+            '§m': 'text-decoration:line-through',
+            '§n': 'text-decoration:underline',
+            '§o': 'font-style:italic'
+        };
 
-    var styleMap = {
-        '§0': 'color:#000000',
-        '§1': 'color:#0000AA',
-        '§2': 'color:#00AA00',
-        '§3': 'color:#00AAAA',
-        '§4': 'color:#AA0000',
-        '§5': 'color:#AA00AA',
-        '§6': 'color:#FFAA00',
-        '§7': 'color:#AAAAAA',
-        '§8': 'color:#555555',
-        '§9': 'color:#5555FF',
-        '§a': 'color:#55FF55',
-        '§b': 'color:#55FFFF',
-        '§c': 'color:#FF5555',
-        '§d': 'color:#FF55FF',
-        '§e': 'color:#FFFF55',
-        '§f': 'color:#FFFFFF',
-        '§l': 'font-weight:bold',
-        '§m': 'text-decoration:line-through',
-        '§n': 'text-decoration:underline',
-        '§o': 'font-style:italic'
-    };
-
-    function obfuscate(string, elem) {
-        var magicSpan,
-            currNode;
-        if (string.indexOf('<br>') > -1) {
-            elem.innerHTML = string;
-            for (var j = 0, len = elem.childNodes.length; j < len; j++) {
-                currNode = elem.childNodes[j];
-                if (currNode.nodeType === 3) {
-                    magicSpan = document.createElement('span');
-                    magicSpan.innerHTML = currNode.nodeValue;
-                    elem.replaceChild(magicSpan, currNode);
-                    init(magicSpan);
-                }
-            }
-        } else {
-            init(elem, string);
-        }
-
-        function init(el, str) {
-            var i = 0,
-                obsStr = str || el.innerHTML,
-                len = obsStr.length;
-            
-            obfuscators[_id].push(
-                window.setInterval(function () {
-                    if (i >= len) i = 0;
-                    obsStr = replaceRand(obsStr, i);
-                    el.innerHTML = obsStr;
-                    i++;
-                }, 0)
-            );
-        }
+    function obfuscate(elem, string) {
+        var multiMagic,
+            currNode,
+            listLen,
+            nodeI;
 
         function randInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -68,43 +42,82 @@
             var randChar = String.fromCharCode(randInt(64, 95));
             return string.substr(0, i) + randChar + string.substr(i + 1, string.length);
         }
+
+        function initMagic(el, str) {
+            var i = 0,
+                obsStr = str || el.innerHTML,
+                strLen = obsStr.length;
+            if(!strLen) return;
+            obfuscators[currId].push(
+                window.setInterval(function () {
+                    if (i >= strLen) i = 0;
+                    obsStr = replaceRand(obsStr, i);
+                    el.innerHTML = obsStr;
+                    i++;
+                }, 0)
+            );
+        }
+
+        if (string.indexOf('<br>') > -1) {
+            elem.innerHTML = string;
+            listLen = elem.childNodes.length;
+            for (nodeI = 0; nodeI < listLen; nodeI++) {
+                currNode = elem.childNodes[nodeI];
+                if (currNode.nodeType === 3) {
+                    multiMagic = document.createElement('span');
+                    multiMagic.innerHTML = currNode.nodeValue;
+                    elem.replaceChild(multiMagic, currNode);
+                    initMagic(multiMagic);
+                }
+            }
+        } else {
+            initMagic(elem, string);
+        }
     }
 
     function applyCode(string, codes) {
         var elem = document.createElement('span'),
             obfuscated = false;
-        string = string.replace(/\x00*/g, '');
-        for (var i = 0, len = codes.length; i < len; i++) {
-            elem.style.cssText += styleMap[codes[i]] + ';';
-            if (codes[i] === '§k') {
-                obfuscate(string, elem);
+
+        string = string.replace(/\x00/g, '');
+
+        codes.forEach(function (code) {
+            elem.style.cssText += styleMap[code] + ';';
+            if (code === '§k') {
+                obfuscate(elem, string);
                 obfuscated = true;
             }
-        }
+        });
+
         if (!obfuscated) elem.innerHTML = string;
+
         return elem;
     }
 
     function parseStyle(string) {
-        var codes = string.match(/§.{1}/g) || [],
+        var finalPre = document.createElement('pre'),
+            codes = string.match(/§.{1}/g) || [],
+            codesLen = codes.length,
             indexes = [],
+            indexDelta,
             apply = [],
-            tmpStr,
-            deltaIndex,
-            finalPre = document.createElement('pre'),
+            strSlice,
             i;
-        
-        if (!obfuscators[_id]) obfuscators[_id] = [];
+
+        if (!obfuscators[currId]) obfuscators[currId] = [];
+
         string = string.replace(/\n|\\n/g, '<br>');
 
-        for (i = 0, len = codes.length; i < len; i++) {
+        for (i = 0; i < codesLen; i++) {
             indexes.push(string.indexOf(codes[i]));
             string = string.replace(codes[i], '\x00\x00');
         }
+
         if (indexes[0] !== 0) {
             finalPre.appendChild(applyCode(string.substring(0, indexes[0]), []));
         }
-        for (i = 0; i < len; i++) {
+
+        for (i = 0; i < codesLen; i++) {
             indexDelta = indexes[i + 1] - indexes[i];
             if (indexDelta === 2) {
                 while (indexDelta === 2) {
@@ -119,27 +132,27 @@
             if (apply.lastIndexOf('§r') > -1) {
                 apply = apply.slice(apply.lastIndexOf('§r') + 1);
             }
-            tmpStr = string.substring(indexes[i], indexes[i + 1]);
-            finalPre.appendChild(applyCode(tmpStr, apply));
+            strSlice = string.substring(indexes[i], indexes[i + 1]);
+            finalPre.appendChild(applyCode(strSlice, apply));
         }
+
         return finalPre;
     }
 
     function clearObfuscators(id) {
-        var i = obfuscators[id].length;
-        for (; i--;) {
-            clearInterval(obfuscators[id][i]);
-        }
+        obfuscators[id].forEach(function (interval) {
+            clearInterval(interval);
+        });
         alreadyParsed[id] = [];
         obfuscators[id] = [];
     }
 
     window.mineParse = function initParser(input) {
         var parsed,
-            i = _id;
-        if(i > 0) {
-            for (; i--;) {
-                if(alreadyParsed[i].nodeType) {
+            i = currId;
+        if (i > 0) {
+            while (i--) {
+                if (alreadyParsed[i].nodeType) {
                     if (!document.contains(alreadyParsed[i])) {
                         clearObfuscators(i);
                     }
@@ -148,7 +161,7 @@
         }
         parsed = parseStyle(input);
         alreadyParsed.push(parsed);
-        _id++;
+        currId++;
         return {
             parsed: parsed,
             raw: parsed.innerHTML
